20dd0bc8f653ee8781d001bb6bc304cf12520a16
Sujan <acharsujan@gmail.com>
2019-02-20 21:18:27 +0530

    Fifth commit

---
+++ day3.1.py
@@ -0,0 +1,14 @@
+# selection sort
+import sys 
+A = [64, 25, 12, 22, 11, 26, 40, 14]  
+for i in range(len(A)):  
+    min_idx = i 
+    for j in range(i+1, len(A)): 
+        if A[min_idx] > A[j]: 
+            min_idx = j         
+    A[i], A[min_idx] = A[min_idx], A[i] 
+  
+# Driver code to test above 
+print ("Sorted array") 
+for i in range(len(A)): 
+    print("%d" %A[i])
