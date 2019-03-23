e5d8b47de6b9cd2d54ef24385c1cb7adfff4f161
Sujan <acharsujan@gmail.com>
2019-03-23 23:06:47 +0530

    first commit

--- day32.py
+++ day32.py
@@ -1,1 +1,29 @@
 #Code to find largest 3 elements in an array 
+import sys
+def printthreelargest(arr, arr_size):
+	if (arr_size < 3):
+		print ("Invalid input")
+		return
+
+	third = first = second = -sys.maximize
+
+	for i in range(0, arr_size):
+		if (arr[i] > first):
+			third = second
+			second = first
+			first = arr[i]
+
+		elif (arr[i] > second):
+			third = second
+			second = arr[i]
+
+		elif (arr[i] > third):
+			third = arr[i]
+
+	print("Three largest elements are", first, second, third)
+
+
+#Driver program
+arr = [12, 13, 1, 10, 34, 1]
+n = len(arr)
+printthreelargest(arr,n)
